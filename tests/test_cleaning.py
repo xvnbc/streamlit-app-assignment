958a8177644d96c6e1710f979281157d58a22e56
Yuqi Xin <yuqixin03@gmail.com>
2026-09-17 14:51:19 +0200

    modify import order

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import pytest
+from conftest import make_raw
 
 from assignment_streamlit_app import cleaning
-from tests.conftest import NaN, make_raw
+
+NaN = np.nan
 
 
 def _row(df, vehicle, start):
